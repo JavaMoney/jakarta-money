edb900d154a8304df8a87d3374789cbf83c0e3c1
Otavio Santana <otaviopolianasantana@gmail.com>
2022-04-13 07:47:38 +0100

    Create package-info in validation
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ src/main/java/org/javamoney/jakarta/validator/package-info.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2022, Otavio Santana and others by the @author tag.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ *  the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations under
+ *  the License.
+ *
+ */
+
+/**
+ * Jakarta Bean Validation is a Java specification which:
+ * * Lets you express constraints on object models via annotations
+ * * Lets you write custom constraints in an extensible way
+ * * Provides the APIs to validate objects and object graphs
+ * * Provides the APIs to validate parameters and return values of methods and constructors
+ * * Reports the set of violations (localized)
+ */
+package org.javamoney.jakarta.validator;
