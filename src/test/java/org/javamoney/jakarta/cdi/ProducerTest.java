7224fb83cd3ab5b077fd06d7020e4a0ef8c7d01f
Otavio Santana <otaviopolianasantana@gmail.com>
2022-04-13 11:10:05 +0100

    create producertest
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/test/java/org/javamoney/jakarta/cdi/ProducerTest.java
+++ src/test/java/org/javamoney/jakarta/cdi/ProducerTest.java
@@ -1,2 +1,46 @@
-package org.javamoney.jakarta.cdi;public class ProducerTest {
+/*
+ * Copyright (c) 2022, Otavio Santana and others by the @author tag.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ *  the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations under
+ *  the License.
+ *
+ */
+
+package org.javamoney.jakarta.cdi;
+
+import org.javamoney.jakarta.test.CDIExtension;
+import org.javamoney.moneta.function.MonetaryAmountProducer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+import javax.money.CurrencyUnit;
+import javax.money.MonetaryAmount;
+
+@CDIExtension
+public class ProducerTest {
+
+    @Inject
+    @MonetaryProducer
+    private MonetaryAmountProducer producer;
+
+    @Inject
+    @Currency("EUR")
+    private CurrencyUnit currency;
+
+    @Test
+    public void shouldCreateMonetaryAmount() {
+
+        MonetaryAmount amount = producer.create(currency, 10);
+        Assertions.assertNotNull(amount);
+    }
 }
