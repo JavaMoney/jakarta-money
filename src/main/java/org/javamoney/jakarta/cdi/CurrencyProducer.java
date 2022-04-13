adb98a721ae4b7d50e1f0682c2ef9761f4b0a8e0
Otavio Santana <otaviopolianasantana@gmail.com>
2022-04-13 08:01:28 +0100

    Refactoring the currency producer
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/org/javamoney/jakarta/cdi/CurrencyProducer.java
+++ src/main/java/org/javamoney/jakarta/cdi/CurrencyProducer.java
@@ -18,22 +18,33 @@
 package org.javamoney.jakarta.cdi;
 
 import javax.enterprise.inject.Default;
+import javax.enterprise.inject.Instance;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 import javax.money.CurrencyUnit;
 import javax.money.Monetary;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Locale;
 
 class CurrencyProducer {
 
-	@Inject
-	private HttpServletRequest request;
+    @Inject
+    private Instance<HttpServletRequest> request;
 
-	@Produces
-	@Default
-	public CurrencyUnit getDefault() {
-		return Monetary.getCurrency(request.getLocale());
-	}
+    @Produces
+    @Default
+    public CurrencyUnit getDefault() {
+        Locale locale = getLocale();
+        return Monetary.getCurrency(locale);
+    }
+
+    private Locale getLocale() {
+        if (request.isResolvable()) {
+            HttpServletRequest servletRequest = request.get();
+            return servletRequest.getLocale();
+        }
+        return Locale.getDefault();
+    }
 
 
 }
